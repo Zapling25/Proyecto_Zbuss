50048f7be7c44215f0e93312c1444253c02501ee
DESKTOP-1NF3EJV\Leogladeador <leogladeador85@gmail.com>
2022-09-21 12:31:43 -0500

    se agrega el proyecto

---
+++ Zbuss_Proyect/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Zbuss_Proyect.Models;
+
+namespace Zbuss_Proyect.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Something> somethings = new List<Something>();
+            somethings.Add(new Something()
+            {
+                Id = 1,
+                Title = "Some 1",
+                Img = "ZBUSS4.jpg"
+
+            });
+            somethings.Add(new Something()
+            {
+                Id = 2,
+                Title = "Some 2",
+                Img = "ZBUSS5.jpg"
+
+            });
+            somethings.Add(new Something()
+            {
+                Id = 3,
+                Title = "Some 3",
+                Img = "ZBUSS6.jpg"
+
+            });
+            return View(somethings);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
